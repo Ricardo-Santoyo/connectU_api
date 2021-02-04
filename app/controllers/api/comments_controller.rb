69fdd4db663322c11a5b65b8b0e696ea3fef5ce9
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-02-03 21:06:25 -0600

    added index action to comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,14 @@
 class Api::CommentsController < Api::BaseController
 
+  def index
+    if params[:post_id]
+      @comments = get_post.comments
+    elsif params[:user_id]
+      @comments = get_user.comments
+    end
+    render json: {data: @comments}, status: :ok
+  end
+
   def create
     @comment = current_user.comments.build(comment_params)
     if @comment.save
@@ -8,6 +17,14 @@ class Api::CommentsController < Api::BaseController
   end
 
   private
+
+  def get_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def get_user
+    @user = User.find(params[:user_id])
+  end
 
   def comment_params
     params.require(:comment).permit(:body, :post_id)
--- spec/controllers/api/comments_controller_spec.rb
+++ spec/controllers/api/comments_controller_spec.rb
@@ -30,4 +30,66 @@ describe Api::CommentsController, type: :request do
       expect(json['data']['body']).to eq(@body)
     end
   end
+
+  context 'When fetching all comments from a user' do
+    before :each do
+      @post1 = create_post(create_user)
+      create_comment(create_user, @post1)
+      create_comment(user, @post1)
+      @post2 = create_post(create_user)
+      create_comment(user, @post2)
+      @post3 = create_post(create_user)
+      create_comment(user, @post3)
+      login_with_api(user)
+      get "/api/comments", headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        user_id: user.id
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'only returns the specified user\'s comments' do
+      expect(json['data'].length()).to be(3)
+      expect(json['data'][0]['user_id']).to be(user.id)
+      expect(json['data'][0]['post_id']).to be(@post1.id)
+      expect(json['data'][1]['user_id']).to be(user.id)
+      expect(json['data'][1]['post_id']).to be(@post2.id)
+      expect(json['data'][2]['user_id']).to be(user.id)
+      expect(json['data'][2]['post_id']).to be(@post3.id)
+    end
+  end
+
+  context 'When fetching all comments from a post' do
+    before :each do
+      @post1 = create_post(user)
+      @post2 = create_post(user)
+      create_comment(create_user, @post1)
+      create_comment(create_user, @post1)
+      create_comment(create_user, @post1)
+      create_comment(create_user, @post2)
+      create_comment(user, @post1)
+
+      login_with_api(user)
+      get "/api/comments", headers: {
+        'Authorization': response.headers['Authorization']
+      }, params: {
+        post_id: @post1.id
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'only returns the specified post\'s comments' do
+      expect(json['data'].length()).to be(4)
+      expect(json['data'][0]['post_id']).to be(@post1.id)
+      expect(json['data'][1]['post_id']).to be(@post1.id)
+      expect(json['data'][2]['post_id']).to be(@post1.id)
+    end
+  end
 end
