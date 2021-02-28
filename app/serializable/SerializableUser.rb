f6f5fc262a1c72e01510b8efa5550f2487ef6c15
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-02-27 18:57:11 -0600

    added created_at attribute to users json response

--- app/serializable/SerializableUser.rb
+++ app/serializable/SerializableUser.rb
@@ -1,7 +1,7 @@
 class SerializableUser < JSONAPI::Serializable::Resource
   type 'users'
 
-  attributes :name, :email, :handle
+  attributes :name, :email, :handle, :created_at
 
   link :self do
     @url_helpers.api_user_url(@object.id)
