bd0c5399d85a9fa1aaa9b72b0efcb6520fec94ff
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-07-10 21:44:00 -0500

    refactored code in post and comment models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,33 +5,8 @@ class Comment < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :reposts, as: :repostable, dependent: :destroy
-  attr_accessor :uid
 
-  private
-
-  def user_name
-    return self.user.name
-  end
-
-  def user_handle
-    return self.user.handle
-  end
- 
-  def comment_count
-    return self.comments.count
-  end
-
-  def commented
-    return self.comments.exists?(user_id:uid)
-  end
-
-  def like_count
-    return self.likes.count
-  end
-
-  def like_id
-    return self.likes.where(user_id:uid).ids.first
-  end
+  include PostInfo
 
   def user_post_id
     return self.user.comments.find_index(self) + 1
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,35 +4,6 @@ class Post < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :reposts, as: :repostable, dependent: :destroy
-  attr_accessor :uid
-
-  private
-
-  def user_name
-    return self.user.name
-  end
-
-  def user_handle
-    return self.user.handle
-  end
-
-  def comment_count
-    return self.comments.count
-  end
-
-  def commented
-    return self.comments.exists?(user_id:uid)
-  end
-
-  def like_count
-    return self.likes.count
-  end
-
-  def like_id
-    return self.likes.where(user_id:uid).ids.first
-  end
-
-  def user_post_id
-    return self.user.posts.find_index(self) + 1
-  end
+  
+  include PostInfo
 end
