bcde145f946517b9b4a0e30f7f22b1d49f200257
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2021-07-19 22:27:02 -0500

    added index action in bookmarks_controller.rb

--- app/controllers/api/bookmarks_controller.rb
+++ app/controllers/api/bookmarks_controller.rb
@@ -2,10 +2,26 @@ class Api::BookmarksController < Api::BaseController
 
   include PostInfoController
 
+  def index
+    @bookmarks = current_user.bookmarks.order(id: :desc).limit(20)
+    @posts = get_posts(@bookmarks)
+    render json: {bookmark: @bookmarks, data: include_post_info(@posts, "index")}, status: :ok
+  end
+
   def create
     @bookmark = current_user.bookmarks.build(bookmarkable_type: params[:type].capitalize, bookmarkable_id: params[:bookmarkable_id])
     if @bookmark.save
       render json: {bookmark: @bookmark, data: include_post_info(@bookmark.bookmarkable) }
     end
+  end
+
+  private
+
+  def get_posts(data)
+    new_data = []
+    data.each do |bookmark|
+      new_data << bookmark.bookmarkable
+    end
+    return new_data
   end
 end
--- spec/controllers/api/bookmarks_controller_spec.rb
+++ spec/controllers/api/bookmarks_controller_spec.rb
@@ -40,4 +40,37 @@ describe Api::BookmarksController, type: :request do
       expect(json['data']['bookmark_id']).to be(json['bookmark']['id'])
     end
   end
+
+  context 'When fetching all bookmarks from a user' do
+    before :each do
+      @user2 = create_user
+      create_bookmark(@user2, create_post(user))
+      create_bookmark(user, create_post(user))
+      @bookmark = create_bookmark(user, create_post(@user2))
+      create_bookmark(user, create_post(user))
+      login_with_api(user)
+      get "/api/bookmarks", headers: {
+        'Authorization': response.headers['Authorization']
+      }
+    end
+
+    it 'returns 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'only returns the specified user\'s bookmarks' do
+      expect(json['data'].length()).to be(3)
+      expect(json['data'][1]['user_name']).to eq(@user2.name)
+      expect(json['data'][1]['user_handle']).to eq(@user2.handle)
+      expect(json['data'][1]['comment_count']).to be(0)
+      expect(json['data'][1]['repost_count']).to be(0)
+      expect(json['data'][1]['repost_id']).to be(nil)
+      expect(json['data'][1]['like_count']).to be(0)
+      expect(json['data'][1]['like_id']).to be(nil)
+      expect(json['data'][1]['bookmark_id']).to be(@bookmark.id)
+      expect(json['data'][0]['user_id']).to be(user.id)
+      expect(json['data'][1]['user_id']).to be(@user2.id)
+      expect(json['data'][2]['user_id']).to be(user.id)
+    end
+  end
 end
